fdb2fbfc1243da41df1236edeb68b2b48909acd6
Nury <nury@Nurys-MacBook-Pro.local>
2019-04-21 22:26:08 +0300

    Initial Commit

---
+++ MyAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppStore
+//
+//  Created by Nury on 4/21/19.
+//  Copyright © 2019 Nury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
